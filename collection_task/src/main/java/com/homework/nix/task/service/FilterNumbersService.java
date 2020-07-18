8f5147b8943d0809dd01379434ca2ead0cc92baa
Nikita-Poltavets <mykyta.poltavets@nure.ua>
2020-07-18 15:06:24 +0300

    Create class service and class implementation for first task

---
+++ collection_task/src/main/java/com/homework/nix/task/service/FilterNumbersService.java
@@ -0,0 +1,4 @@
+package com.homework.nix.task.service;
+
+public interface FilterNumbersService {
+}
---
+++ collection_task/src/main/java/com/homework/nix/task/service/impl/FilterNumbersServiceImpl.java
@@ -0,0 +1,6 @@
+package com.homework.nix.task.service.impl;
+
+import com.homework.nix.task.service.FilterNumbersService;
+
+public class FilterNumbersServiceImpl implements FilterNumbersService {
+}
