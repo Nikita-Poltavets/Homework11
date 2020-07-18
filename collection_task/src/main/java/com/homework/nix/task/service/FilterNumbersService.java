c7d28e34b5b2195c174e1c84536c4ec6fbf7a8c6
Nikita-Poltavets <mykyta.poltavets@nure.ua>
2020-07-18 19:33:44 +0300

    Create implementation for filtering numbers in list of strings. First task done.

--- collection_task/src/main/java/com/homework/nix/task/service/FilterNumbersService.java
+++ collection_task/src/main/java/com/homework/nix/task/service/FilterNumbersService.java
@@ -1,4 +1,8 @@
 package com.homework.nix.task.service;
 
+import java.util.List;
+
 public interface FilterNumbersService {
+
+    void filterNumbersInCollection(List<String> stringList);
 }
--- collection_task/src/main/java/com/homework/nix/task/service/impl/FilterNumbersServiceImpl.java
+++ collection_task/src/main/java/com/homework/nix/task/service/impl/FilterNumbersServiceImpl.java
@@ -2,5 +2,20 @@ package com.homework.nix.task.service.impl;
 
 import com.homework.nix.task.service.FilterNumbersService;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class FilterNumbersServiceImpl implements FilterNumbersService {
+
+    @Override
+    public void filterNumbersInCollection(List<String> stringList) {
+
+        List<String> allNumbersStringList = stringList.stream().map(s -> s.replaceAll("\\D+","")).collect(Collectors.toList());
+
+        String stringOfNumbers = String.join("", allNumbersStringList);
+
+        int allNumbers = Integer.parseInt(stringOfNumbers);
+
+        System.out.println(allNumbers);
+    }
 }
